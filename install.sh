0b42aaf389b0239d96741e950205c48f4adead20
Yao Yao <90464347+Zoeyyao27@users.noreply.github.com>
2024-03-28 10:37:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ pip install cython==0.29 --upgrade
 pip install -e .
 pip install stanford-openie
 pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
+
+cd ..
 pip install -r requirements.txt
